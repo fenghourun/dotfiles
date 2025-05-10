1918fde2deb1d0a8a1a9e76129661b9fd15e2318
David Feng <fenghourun@gmail.com>
2025-05-10 03:59:08 -0400

    GReen

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 export WHITE=0xffffffff
 # Use this for rounded look
-# export BACKGROUND_COLOR=0x00f17013
-export BACKGROUND_COLOR=0xff1e362f
-export ITEM_BG_COLOR=0xff1e362f
+export BACKGROUND_COLOR=0x00f17013
+# export BACKGROUND_COLOR=0xff1e362f
+# export ITEM_BG_COLOR=0xff1e362f
+export ITEM_BG_COLOR=0xff2c4941
+
 export ACCENT_COLOR=0xff8cd2a0
 export TRANSPARENT=0x00f17013
 export BORDER_COLOR=0xfff34b00
