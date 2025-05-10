376e09ae893506b9b5ed620ff8ec713ed8caee82
David Feng <fenghourun@gmail.com>
2025-05-10 03:53:57 -0400

    Update colors

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
 export WHITE=0xffffffff
-export BACKGROUND_COLOR=0x00f17013
-export ITEM_BG_COLOR=0xff1e2021
-export ACCENT_COLOR=0xfff08978
+# Use this for rounded look
+# export BACKGROUND_COLOR=0x00f17013
+export BACKGROUND_COLOR=0xff1e362f
+export ITEM_BG_COLOR=0xff1e362f
+export ACCENT_COLOR=0xff8cd2a0
 export TRANSPARENT=0x00f17013
 export BORDER_COLOR=0xfff34b00
